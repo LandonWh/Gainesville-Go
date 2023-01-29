bf97304519418b29602196219ad1dbc613205b42
McclellandKmra <mcclellandkmra@gmail.com>
2023-01-29 13:28:12 -0500

    Components and fixes
    Added login and registration components.
    Fixed an issue with the baseUrl option in tsconfig.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { MatInputModule } from '@angular/material/input';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    RegisterComponent
   ],
   imports: [
     MatSlideToggleModule,
