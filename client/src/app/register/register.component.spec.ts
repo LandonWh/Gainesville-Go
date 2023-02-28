4c9753e6a845c1aaf109a1b703dcd6bfe36bd876
McclellandKmra <mcclellandkmra@gmail.com>
2023-02-28 17:18:15 -0500

    Added Unit Tests
    -Register

--- client/src/app/register/register.component.spec.ts
+++ client/src/app/register/register.component.spec.ts
@@ -1,20 +1,47 @@
+//Unit Tests from https://tekloon.medium.com/angular-unit-testing-login-c39da80ae696
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing'
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { RegisterComponent } from './register.component';
 import { HttpClientModule } from '@angular/common/http';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
+import { RegisterComponent } from './register.component'
+import { AuthService } from '../auth.service';
 import { ReactiveFormsModule } from '@angular/forms';
+import { blankUser, validUser } from 'src/mocks';
+
+const registerServiceSpy = jasmine.createSpyObj('RegisterService', ['register']);
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
 
+  function updateForm(firstName: string, lastName: string, email: string, password: string) {
+    component.registerForm.controls['firstName'].setValue(firstName);
+    component.registerForm.controls['lastName'].setValue(lastName);
+    component.registerForm.controls['email'].setValue(email);
+    component.registerForm.controls['password'].setValue(password);
+  }
+
   beforeEach(async () => {
+
+
     await TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ],
-      imports: [HttpClientModule, HttpClientTestingModule, ReactiveFormsModule],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
-    }).compileComponents();
+      imports: [
+        HttpClientTestingModule,
+        ReactiveFormsModule
+      ],
+      declarations: [ 
+        RegisterComponent
+      ],
+      providers: [
+        {provide: AuthService, userValue: {}},
+        
+      ],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
+      ]
+    })
+    .compileComponents();
 
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
@@ -25,8 +52,22 @@ describe('RegisterComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('component initial state', () => {
+    expect(component.added).toBeFalsy();
+    expect(component.registerForm).toBeDefined();
+    expect(component.registerForm.invalid).toBeTruthy();
+    expect(component.authError).toBeFalsy();
+    expect(component.authErrorMsg).toBeUndefined();
+  });
 
-  
+  it('submitted should be true when addAccount()', () => {
+    component.onSubmit(blankUser);
+    expect(component.added).toBeTruthy();
+    expect(component.authError).toBeFalsy();
+  });
 
-
+  it('form value should update from when u change the input', (() => {
+    updateForm(validUser.firstName, validUser.lastName, validUser.email, validUser.password);
+    expect(component.registerForm.value).toEqual(validUser);
+  }));
 });
---
+++ client/src/mocks/index.ts
@@ -0,0 +1,13 @@
+export const blankUser = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: ''
+}
+
+export const validUser = {
+    firstName: 'John',
+    lastName: 'Smith',
+    email: 'j.smith@gmail.com',
+    password: 'password'
+}
